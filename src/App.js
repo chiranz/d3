0c1df8365e703255ab24d441f8f665441c2d711a
chiranz <chiranpoudyal@gmail.com>
2020-03-09 13:03:23 +0530

    showing x scale and y scale

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import { select, line, curveCardinal } from "d3";
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  axisRight,
+  scaleLinear,
+  max
+} from "d3";
 
 function App() {
   const [data, setData] = useState([20, 12, 25, 35, 50, 75, 80]);
@@ -9,22 +17,50 @@ function App() {
     `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
   useEffect(() => {
     const svg = select(svgRef.current);
-    const myLine = line()
-      .x((_, index) => index * 50)
-      .y(value => 150 - value)
-      .curve(curveCardinal);
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+    const yScale = scaleLinear()
+      .domain([0, max(data)])
+      .range([150, 0]);
+
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat(index => index + 1);
+    const yAxis = axisRight(yScale);
     svg
-      .selectAll("path")
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(300px)")
+      .call(yAxis);
+    const myLine = line()
+      .x((_, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal);
+
+    svg
+      .selectAll(".line")
       .data([data])
       .join("path")
-      .attr("d", value => myLine(value))
+      .attr("class", "line")
+      .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", randomColor());
   }, [data]);
   return (
     <div className="App">
       <h1 style={{ color: randomColor }}>D3 here</h1>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis"></g>
+        <g className="y-axis"></g>
+      </svg>
+
+      <br />
+      <br />
+      <br />
       <button onClick={() => setData(data => data.map(val => val + 3))}>
         Update data
       </button>
