dfe1cac8043d0b32dc75535eb5fa30cb027fb570
chiranz <chiranpoudyal@gmail.com>
2020-03-09 11:37:00 +0530

    create update and remove circles using d3 and react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import { select } from "d3";
 
 function App() {
+  const [data, setData] = useState([20, 12, 25, 35, 50]);
+  const svgRef = useRef();
+  const randomColor = () =>
+    `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join(
+        enter => enter.append("circle"),
+        update => update.attr("class", "updated")
+      )
+      .attr("r", value => value)
+      .attr("cx", value => value * 2)
+      .attr("cy", value => value * 2)
+      .attr("stroke", randomColor());
+  }, [data]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: randomColor }}>D3 here</h1>
+      <svg ref={svgRef}></svg>
+      <button onClick={() => setData(data => data.map(val => val + 3))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data => data.filter(val => val < 38))}>
+        Filter data
+      </button>
     </div>
   );
 }
